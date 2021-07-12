7f5f544ebb0b55055c3e7e3b6b394ecf6b4406c8
Javiistacks <javiann.marrero@icloud.com>
2021-07-11 19:44:20 -0500

    initial commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Full Stack Developer",
+          "Interested in Front End",
+          "Interested in UX/UI",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
